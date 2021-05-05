12670b6d4276975700e7e51b3372ea92d18ff3a6
George Cope <G.Cope13@gmail.com>
2021-05-05 14:30:59 +0100

    Added comments throughout so i know what needs to be added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import react
+// import components
+// import & install axios npm package to handle API calls
+
+
 function App() {
+  // Declare states
+
+  // GET request to API
+
+  // Function to search
+
+  // Function to sort table results in ascending order
+
+  // Function to sort table results in descending order
+
+  // Function to handle whether to call ascending or descending sort function
+
+
   return (
+    // Replace this with what should actually be rendered
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import & install css framework package
 
 ReactDOM.render(
   <React.StrictMode>
