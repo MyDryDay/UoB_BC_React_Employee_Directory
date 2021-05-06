2846b7c6bec5a8750f8010547329eb6feaedbb97
George Cope <G.Cope13@gmail.com>
2021-05-06 05:23:18 +0100

    Created Header component and rendered it in App.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,16 @@
 // import react
-
+import React from 'react';
 // Create functional header component
 // Navbar maybe?
+function Header(){
+    return(
+        <nav className='navbar navbar-dark bg-dark justify-content-center'>
+            <span className='navbar-brand mb-01 h1'>
+                React Employee Directory
+            </span>
+        </nav>
+    )
+}
 
 // export Header as default
+export default Header;
