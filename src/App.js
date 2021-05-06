dfdf4fd8623e28cca65676ef81d60230df161267
George Cope <G.Cope13@gmail.com>
2021-05-06 05:47:30 +0100

    Declared states, defined API call within componentDidMount, created fucntion to search through results of API call by title, firstname or lastname

--- src/App.js
+++ src/App.js
@@ -9,10 +9,38 @@ import axios from 'axios';
 
 class App extends React.Component {
   // Declare states
+  state = {
+    employees: {},
+    filteredList: []
+  }
 
-  // GET request to API
+  // GET request to API for user data
+  async componentDidMount(){
+    // Set API URL to a variable
+    let API = "https://randomuser.me/api/?results=25";
+
+    // Call GET on API
+    axios.get(API).then((res) => {
+      // Once GET request is succesful, set states
+      this.setState({
+        employees: res.data.results,
+        filteredList: res.data.results
+      })
+    })
+  }
 
   // Function to search
+  search = (query) => {
+    // Filters the employees array and creates a new one; filtered
+    const filtered = this.state.employees.filter((employee) => {
+      // Create a variable called fullName that holds any employee's whole name
+      const fullName = (`${employee.name.title} ${employee.name.first} ${employee.name.last}`).toLowerCase();
+      // Returns any employees whose names include the query term
+      // So employees can be searched by title, first name or last name
+      return fullName.includes(query);
+    })
+    this.setState({filteredList: filtered});
+  }
 
   // Function to sort table results in ascending order
 
