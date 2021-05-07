751ca94674d16c5562ca5a9458f50743a64d1abe
George Cope <G.Cope13@gmail.com>
2021-05-07 13:50:38 +0100

    Created the search component & rendered it in Header.js, will add functionality next

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 // import react
 import React from 'react';
+import Search from './Search';
 // Create functional header component
 // Navbar maybe?
 function Header(){
-    return(
-        <nav className='navbar navbar-dark bg-dark justify-content-center'>
+    return (
+        <nav className='navbar navbar-dark bg-dark justify-content-left'>
             <span className='navbar-brand mb-01 h1'>
                 React Employee Directory
             </span>
+            <Search />
         </nav>
     )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,17 @@
 // import react
-
+import React from 'react';
 // Create functional Search component
 // Function to search data in the table
+function Search(props){
 
+    return (
+        <form className='d-flex'>
+            <input className='form-control me-2' type='search' placeholder='Search...' aria-label='Search'></input>
+            <button className='btn btn-outline-primary' type='submit'>Search</button>
+        </form>
+    )
+}
 // Potentially have this component combined with Header?
 
 // export Search as default
+export default Search;
