e4e2b847c040cddd841a9a7aa8e498b7f009c292
George Cope <G.Cope13@gmail.com>
2021-05-09 15:55:31 +0100

    reverting changes

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+// import react
+
+// Create functional footer component
+// Links to github maybe?
+
+// export Footer as default
---
+++ src/components/TableRow.js
@@ -0,0 +1,19 @@
+// import react
+import React from 'react';
+// Create functional TableRow component
+function TableRow(props){
+    return (
+        <tr>
+            <td>
+                <img src={props.picture.thumbnail} alt="Employee"></img>
+            </td>
+            <td>{`${props.name.title} ${props.name.first} ${props.name.last}`} </td>
+            <td>{props.dob.age}</td>
+            <td>{props.location.country}</td>
+            <td>{props.email}</td>
+            <td>{props.phone}</td>
+        </tr>
+    )
+}
+// export TableRow as default
+export default TableRow;
